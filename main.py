4326b31d47397d5ae03b86664b179161cfc3e370
Eduarda Michaelle <78242191+EduardaMichaelle@users.noreply.github.com>
2023-06-07 22:52:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import re
 
-def pre_processador(arquivo_c): #função para ler o arquivo
+#FUNÇÃO PARA EXPANDIR O INCLUDE
+def expandir(linha, novoArquivo):
+  biblioteca = ""
+  if ("#include" in linha):
+    index = linha.find("<") 
+    if (index != -1):
+      biblioteca = linha[index + 1:len(linha.strip()) - 1]
+    index = linha.find("\"")
+    if (index != -1):
+      biblioteca = linha[index + 1:len(linha.strip()) - 1]
+
+    pre_processador(biblioteca, novoArquivo)
+
+#FUNÇÃO QUE SIMULA O PRÉ-PROCESSADOR
+def pre_processador(arquivo_c,ArquivoFinal): #aquivo inicial e arquivo reescrito
   with open(arquivo_c,'r') as file: #abrindo o arquivo e atribuindo-o à variável 'file'
     arquivo = file.readlines() #'arquivo' receberá cada linha dentro de 'file'
     file.close() #fechando o arquivo 'file'
@@ -10,34 +24,36 @@ def pre_processador(arquivo_c): #função para ler o arquivo
   #FOR PARA TIRAR QUEBRA DE LINHAS
   for linha in arquivo: #for que passa em todas as linhas de "arquivo"
     if '\n' in linha: #se a linha for igual a \n 
-      linha = linha.replace('\n','') #mudamos a linha para \nç
+      linha = linha.replace('\n','') #mudamos a linha para vazio
     lista_arquivo.append(linha) #inserindo a linha alterada na nova lista "lista_arquivo"
 
   nova_lista=[] #nova lista auxiliar
   
-  #FOR PARA TIRAR COMENTARIOS 
+  #FOR PARA TIRAR COMENTÁRIOS 
   for linha in lista_arquivo: #for que passa em todas as linhas de "lista_arquivo"
-    if ' ':
+    if ' ' in linha:
       linha=re.sub("^\s+",'', linha) #regex (expressão regular) pega os espaços iniciais (até acabarem) e os substitui por vazio 
-    if '#' in linha:
-      linha = linha + '\n'
+    if '#include' in linha:
+      lista_arquivo = expandir(linha,ArquivoFinal) #expandindo as bibliotecas em include 
+      linha = ''
     if '//' in linha: #se a linha tiver //
       linha = '' #linha receberá vazio
     nova_lista.append(linha) #inserindo a linha alterada na nova lista "nova_lista"
-    
-  arquivo_processado=''.join(nova_lista)
+
+  arquivo_processado=''.join(nova_lista) 
 
   #CRIANDO NOVO ARQUIVO EM C PARA O CÓDIGO JÁ PROCESSADO
-  with open('arquivoProcessado.c','w') as file: 
-    for linha in arquivo_processado:
-      file.write(linha)
-    file.close()
+  for linha in arquivo_processado:
+    ArquivoFinal.write(linha)
     
   return arquivo_processado #retornando o arquivo processado para leitura
 
-#main
+#MAIN
 arquivo_c = 'exemplo.c' #'arquivo_c' recebe o arquivo para ser pré-processado
-arquivo_processado = pre_processador(arquivo_c) #variável "arquivo_processado" receberá o retorno da função "pre_processador"
+ArquivoFinal = open('arquivoProcessado.c','w') #CRIANDO UM ARQUIVO
+arquivo_processado = pre_processador(arquivo_c,ArquivoFinal) #variável "arquivo_processado" receberá o retorno da função "pre_processador"
+
+
 
 #IMPRIMINDO O ARQUIVO ANTES DE SER PROCESSADO
 with open(arquivo_c,'r') as file:
@@ -46,3 +62,5 @@ print(read_arquivo_c)
 print('-------------------------------------------------')
 #IMPRIMINDO O ARQUIVO APÓS SER PROCESSADO
 print(arquivo_processado)
+
+ArquivoFinal.close() #FECHANDO O ARQUIVO
